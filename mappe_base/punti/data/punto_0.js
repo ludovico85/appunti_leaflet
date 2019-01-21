f902e9a98accf604296538df1da4e75b48117d7e
ludovico.frate <frateludovico@gmail.com>
2019-01-21 16:47:36 +0100

    opacità

---
+++ mappe_base/punti/data/punto_0.js
@@ -0,0 +1,11 @@
+var json_punto_0 = {
+"type": "FeatureCollection",
+"name": "punto_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 0.0, "tipo": "edificio" }, "geometry": { "type": "Point", "coordinates": [ 14.513558226385602, 41.705783018373765 ] } },
+{ "type": "Feature", "properties": { "id": 1.0, "tipo": "edificio" }, "geometry": { "type": "Point", "coordinates": [ 14.730491613017925, 41.677148026618603 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "tipo": "aereoporto" }, "geometry": { "type": "Point", "coordinates": [ 14.616546803877712, 41.546902895890909 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "tipo": "porto" }, "geometry": { "type": "Point", "coordinates": [ 14.419334634211964, 41.626391897209338 ] } }
+]
+}
