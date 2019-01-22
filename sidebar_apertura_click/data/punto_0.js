e97c0300fbae57abe6934bbf5062eaa781de49f0
ludovico.frate <frateludovico@gmail.com>
2019-01-22 17:33:25 +0100

    update sidebar_apertura_click

--- sidebar_apertura_click/data/punto_0.js
+++ sidebar_apertura_click/data/punto_0.js
@@ -3,9 +3,9 @@ var json_punto_0 = {
 "name": "punto_0",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "object_id": 0.0, "tipo": "edificio", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.513558226385602, 41.705783018373765 ] } },
-{ "type": "Feature", "properties": { "object_id": 1.0, "tipo": "edificio", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.730491613017925, 41.677148026618603 ] } },
-{ "type": "Feature", "properties": { "object_id": 2.0, "tipo": "aereoporto", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.616546803877712, 41.546902895890909 ] } },
-{ "type": "Feature", "properties": { "object_id": 3.0, "tipo": "porto", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.419334634211964, 41.626391897209338 ] } }
+{ "type": "Feature", "properties": { "OBJECTID": 0, "tipo": "edificio", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.513558226385602, 41.705783018373765 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 1, "tipo": "edificio", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.730491613017925, 41.677148026618603 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 2, "tipo": "aereoporto", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.616546803877712, 41.546902895890909 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 3, "tipo": "porto", "Foto":"images/aereoporto.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.419334634211964, 41.626391897209338 ] } }
 ]
 }
