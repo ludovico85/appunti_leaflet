ca6a802a3216a118a98d9a32595c03da8fa20b11
ludovico85 <frateludovico@gmail.com>
2020-04-11 20:18:02 +0200

    11-04-2020

---
+++ prova_leaflet_divisionelayer/qgis2web_2018_06_27-19_17_19_581000/data/Punticasuali_0.js
@@ -0,0 +1,17 @@
+var json_Punticasuali_0 = {
+"type": "FeatureCollection",
+"name": "Punticasuali_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 0.0, "genere": "genere1", "specie": "specie1" }, "geometry": { "type": "Point", "coordinates": [ 13.621337602754995, 41.954829010311563 ] } },
+{ "type": "Feature", "properties": { "id": 1.0, "genere": "genere1", "specie": "specie1" }, "geometry": { "type": "Point", "coordinates": [ 13.265976380892969, 42.021768440626353 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "genere": "genere1", "specie": "specie2" }, "geometry": { "type": "Point", "coordinates": [ 13.160397281553278, 41.837138936114151 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "genere": "genere2", "specie": "specie3" }, "geometry": { "type": "Point", "coordinates": [ 13.664319742997137, 41.939451911526909 ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "genere": "genere2", "specie": "specie3" }, "geometry": { "type": "Point", "coordinates": [ 13.724691453308028, 41.667478473176608 ] } },
+{ "type": "Feature", "properties": { "id": 5.0, "genere": "genere2", "specie": "specie4" }, "geometry": { "type": "Point", "coordinates": [ 14.238282657845454, 41.484567010110787 ] } },
+{ "type": "Feature", "properties": { "id": 6.0, "genere": "genere2", "specie": "specie4" }, "geometry": { "type": "Point", "coordinates": [ 13.097713985577908, 41.661038782006344 ] } },
+{ "type": "Feature", "properties": { "id": 7.0, "genere": "genere1", "specie": "specie1" }, "geometry": { "type": "Point", "coordinates": [ 13.472802222488001, 41.758717660504225 ] } },
+{ "type": "Feature", "properties": { "id": 8.0, "genere": "genere1", "specie": "specie2" }, "geometry": { "type": "Point", "coordinates": [ 13.586810039297315, 41.754302882232054 ] } },
+{ "type": "Feature", "properties": { "id": 9.0, "genere": "genere1", "specie": "specie1" }, "geometry": { "type": "Point", "coordinates": [ 14.53737881124631, 41.480860775948059 ] } }
+]
+}
