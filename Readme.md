dfcec5e4ee9d5b25df61e9ed15c2ca8b108319fc
ludovico85 <frateludovico@gmail.com>
2021-06-13 15:31:41 +0200

    simple_goejson

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 Live examples
 
 * [simple marker](http://ludovico85.github.io/appunti_leaflet/simple_marker)
+* [simple geojson](http://ludovico85.github.io/appunti_leaflet/simple_geojson)
---
+++ docs/simple_geojson/js/app.js
@@ -0,0 +1,24 @@
+// intial map settings
+var mymap = L.map('map',
+	{
+		zoomControl:false,//custom zoom control
+    }).setView([41.65, 14.0], 12);
+
+L.control.zoom({
+    position:'topleft'// default is topleft
+}).addTo(mymap);
+
+L.control.scale().addTo(mymap); // add scale bar
+
+var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		  maxZoom: 21,attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		  id: 'mapbox/streets-v11',
+		  tileSize: 512,
+		  zoomOffset: -1}).addTo(mymap); // add scale bar
+
+//var marker = L.marker([41.82,14.3]).addTo(mymap)
+
+var mountains = new L.geoJson(mountains, {
+	onEachFeature: function (feature, layer) {
+		layer.bindPopup('<table><tbody><tr><th scope="row"><td>Nome: '+feature.properties.nome+'</td></th></tr></tbody><tr><th scope="row"></th></tr></tbody>')}
+}).addTo(mymap);
